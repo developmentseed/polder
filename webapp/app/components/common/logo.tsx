34062ce1962cf3dc9ac009a98db2bb4eba518212
Ricardo Mestre <ricardo@duplos.org>
2024-10-16 17:09:26 +0200

    Update app logo

--- webapp/app/components/common/logo.tsx
+++ webapp/app/components/common/logo.tsx
@@ -14,7 +14,15 @@ export default function Logo(props: ChakraProps) {
         xmlns='http://www.w3.org/2000/svg'
         {...props}
       >
-        <circle cx='64' cy='64' r='64' fill={colorPrimary} />
+        <path d='M0 8C0 3.58172 3.58172 0 8 0H56V40H0V8Z' fill='#539772' />
+        <path
+          d='M0 48H56V128H32C14.3269 128 0 113.673 0 96V48Z'
+          fill='#6C7F2A'
+        />
+        <path
+          d='M64 0H96C113.673 0 128 14.3269 128 32V76H64V0Z'
+          fill='#3AAFB9'
+        />
       </chakra.svg>
       <Text fontWeight='800' color='base.500'>
         Poldre
